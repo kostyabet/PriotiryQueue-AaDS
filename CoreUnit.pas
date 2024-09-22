4e4d4325b055a6eef46f0e83d3621012a502f8c0
Kostya <betenya.kostya@mail.ru>
2024-09-22 19:23:31 +0300

    create clear project

---
+++ CoreUnit.pas
@@ -0,0 +1,7 @@
+Unit CoreUnit;
+
+Interface
+
+Implementation
+
+End.
---
+++ Processor.dpr
@@ -0,0 +1,12 @@
+Program Processor;
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+
+uses
+  System.SysUtils,
+  CoreUnit in 'CoreUnit.pas';
+
+Begin
+
+End.
