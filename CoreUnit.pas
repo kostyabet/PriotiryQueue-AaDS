acf95386448035a034fb6256350195c191327cc1
Kostya <betenya.kostya@mail.ru>
2024-09-22 21:00:54 +0300

    create constructor

--- CoreUnit.pas
+++ CoreUnit.pas
@@ -2,6 +2,34 @@ Unit CoreUnit;
 
 Interface
 
+Uses
+    UserUnit,
+    PriorityQueueUnit;
+
+Type
+    TArrayOfUsers = Array Of TUser;
+
+    TCore = Class
+    Var
+        Users: TArrayOfUsers;
+        Queue: TPriorityQueue<TUser>;
+        TickTime: Integer;
+        WaitTime: Integer;
+    Private
+
+    Public
+        Constructor Create(ArrayOfUsers: TArrayOfUsers; TickTime: Integer; WaitTime: Integer);
+    End;
+
 Implementation
+
+{ TCore }
+
+Constructor TCore.Create(ArrayOfUsers: TArrayOfUsers; TickTime, WaitTime: Integer);
+Begin
+    Users := ArrayOfUsers;
+    TickTime := TickTime;
+    WaitTime := WaitTime;
+End;
 
 End.
