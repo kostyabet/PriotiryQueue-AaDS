77ac4d983deb82f0fe0b0c349001e35a0b5fd73f
Kostya <betenya.kostya@mail.ru>
2024-09-22 21:05:27 +0300

    push items in Queue

--- CoreUnit.pas
+++ CoreUnit.pas
@@ -29,15 +29,19 @@ Implementation
 Constructor TCore.Create(ArrayOfUsers: TArrayOfUsers; TickTime, WaitTime: Integer);
 Begin
     Users := ArrayOfUsers;
+    For Var I := 0 To Length(Users) - 1 Do
+    Begin
+        Queue.Enqueue(Users[I], Users[I].PriorityLvl);
+    End;
     TickTime := TickTime;
     WaitTime := WaitTime;
 End;
 
-destructor TCore.Destroy;
-begin
+Destructor TCore.Destroy;
+Begin
     Queue.Destroy();
     Users := Nil;
-    inherited;
-end;
+    Inherited;
+End;
 
 End.
