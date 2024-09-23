2d9afc128c7b0c08398d1bc0c3606b5d83106a5d
Kostya <betenya.kostya@mail.ru>
2024-09-23 17:53:03 +0300

    create Users Array

--- Processor.dpr
+++ Processor.dpr
@@ -3,10 +3,28 @@ Program Processor;
 {$APPTYPE CONSOLE}
 {$R *.res}
 
-uses
-  System.SysUtils,
-  CoreUnit in 'CoreUnit.pas';
+Uses
+    System.SysUtils,
+    CoreUnit In 'CoreUnit.pas',
+    UserUnit In 'UserUnit.pas',
+    PriorityQueueUnit In 'PriorityQueueUnit.pas';
+
+Const
+    COUNT_OF_USERS: Integer = 6;
+
+Var
+    ArrayOfUsers: TArrayOfUsers;
 
 Begin
+    SetLength(ArrayOfUsers, COUNT_OF_USERS);
+    ArrayOfUsers[0] := TUser.Create('1', 1, [9, 6, 3, 2, 8, 7, 4]);
+    ArrayOfUsers[1] := TUser.Create('2', 2, [3, 7, 2, 4, 7, 3, 1]);
+    ArrayOfUsers[2] := TUser.Create('3', 2, [4, 5, 6, 4, 2, 3, 4, 5]);
+    ArrayOfUsers[3] := TUser.Create('4', 2, [3, 5, 4, 2, 1, 6, 8, 3]);
+    ArrayOfUsers[4] := TUser.Create('5', 3, [2, 3, 2, 1, 4, 2, 3, 4]);
+    ArrayOfUsers[5] := TUser.Create('6', 3, [3, 2, 1, 2, 3, 4, 3, 4]);
+
+
+
 
 End.
